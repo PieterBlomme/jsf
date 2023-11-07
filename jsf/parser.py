044009f15ef222f29e4c75515b1bf7a5bd65edd2
Pieter Blomme <pieter.blomme@vito.be>
2023-11-07 08:57:46 +0100

    so what is in self.definitions?

--- jsf/parser.py
+++ jsf/parser.py
@@ -169,9 +169,7 @@ class JSF:
                 logger.warning(schema["$ref"])
                 logger.warning(ext)
                 logger.warning(frag)
-                logger.warning(self.definitions.get(f"#{frag}"))
-                logger.warning(external_jsf)
-                logger.warning(external_jsf.definitions.get(f"#{frag}"))
+                logger.warning(self.definitions)
                 raise ValueError(f"Cannot parse schema {repr(schema)}")  # pragma: no cover
         elif "anyOf" in schema:
             return self.__parse_anyOf(name, path, schema)
